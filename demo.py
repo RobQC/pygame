ebe5dbf6afdf7d843c19236af0539d4987ea0e39
Hector <hector.rz11@gmail.com>
2014-01-27 16:20:08 -0500

    practicando -*- git

--- demo.py
+++ demo.py
@@ -1,4 +1,21 @@
-print "hola mundo"
-print "hola alan"
-print "linares ta aportando xD"
-print "hector raul"
+#! /usr/bin/env python
+# -*- endcoding: utf-8 -*-
+# demo.py - nuestro primer programtica en python XD
+
+import random
+
+numero = random.randint(1,100)
+
+tentativas = 0
+opcion = 0
+
+while opcion != numero:
+	opcion = input("escriba un numero del 1 al 100: ")
+	tentativas += 1
+	if opcion > numero:
+		print "Mayor"
+	elif opcion < numero:
+		print "Menor"
+
+print "Acerto! El numero sorteado era: ", numero
+print "Usted uso ",tentativas," tentativas"
